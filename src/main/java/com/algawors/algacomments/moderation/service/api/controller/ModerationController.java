db272864e156a5b8605a6fdb378f3c43d0e096e4
Vini Silveira <viniciusps_ph@hotmail.com>
2025-07-17 19:46:19 -0300

    - Inserção de Logs

--- src/main/java/com/algawors/algacomments/moderation/service/api/controller/ModerationController.java
+++ src/main/java/com/algawors/algacomments/moderation/service/api/controller/ModerationController.java
@@ -3,6 +3,7 @@ package com.algawors.algacomments.moderation.service.api.controller;
 import com.algawors.algacomments.moderation.service.api.model.ModerationInput;
 import com.algawors.algacomments.moderation.service.api.model.ModerationOutput;
 import com.algawors.algacomments.moderation.service.domain.ForbiddenWords;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,10 @@ import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/moderate")
+@Slf4j
 public class ModerationController {
 
-    private static final String FORBIDDEN_WORD_FOUND = "Comentário negado: Uma palavra proíbida foi encontrada. Favor revisar.";
+    private static final String FORBIDDEN_WORD_FOUND = "Comment Denied: A forbidden word was found. Please, review your comment.";
 
     @PostMapping
     public ModerationOutput approve(@RequestBody ModerationInput moderationInput) {
@@ -26,10 +28,14 @@ public class ModerationController {
 
         String reason = forbiddenWordFound ? FORBIDDEN_WORD_FOUND : null;
 
-        return ModerationOutput.builder()
+        ModerationOutput moderationOutput = ModerationOutput.builder()
                 .approved(!forbiddenWordFound)
                 .reason(reason)
                 .build();
+
+        log.info(moderationOutput.toString());
+
+        return moderationOutput;
     }
 
 }
